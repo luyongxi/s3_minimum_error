87c4f074397e6c40ae2d42d9426defd459deac70
Yongxi Lu <yol070@ucsd.edu>
2018-10-16 19:24:43 +0800

    add minimum example

---
+++ Makefile
@@ -0,0 +1,2 @@
+deploy:
+	cd .. && kubectl cp s3_minimum_error yongxi-login:/nfs/yongxi/repos
---
+++ main.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+import argparse
+import os.path as osp
+
+def load_batch(s3_path):
+
+    with open("filelist.txt", 'r') as f:
+        fnames = f.readlines()
+
+    filelist = [osp.join(s3_path, 'data', fn) for fn in fnames]
+    paths_queue = tf.train.string_input_producer(filelist, shuffle=False)
+    reader = tf.WholeFileReader()
+    keys, contents = reader.read(paths_queue)
+    batch = tf.reshape(tf.decode_raw(contents, tf.float32), [1])
+    batch = tf.train.shuffle_batch([batch], 1, 2, 1)
+
+    return batch
+
+# Please put all the "data" folder to the S3_path
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description="Debugging options.")
+    parser.add_argument('s3_path', type=str, help='path of the S3 endpoint.')
+    args = parser.parse_args()
+
+    with tf.Graph().as_default():
+
+        batch = load_batch(args.s3_path)
+
+        with tf.Session() as sess:
+
+            for i in range(50):
+                print("loading {:d}".format(i))
+                print(sess.run(batch))
