a024f69b34a870e5a8f45b058f0ff846be087839
Yongxi Lu <yol070@ucsd.edu>
2018-10-17 15:50:10 +0800

    fix a bug...still no luck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import argparse
+import os
 import os.path as osp
 
 def load_batch(s3_path):
@@ -7,7 +8,7 @@ def load_batch(s3_path):
     with open("filelist.txt", 'r') as f:
         fnames = f.readlines()
 
-    filelist = [osp.join(s3_path, 'data', fn) for fn in fnames]
+    filelist = [osp.join(s3_path, 'data', fn.strip()) for fn in fnames]
     paths_queue = tf.train.string_input_producer(filelist, shuffle=False)
     reader = tf.WholeFileReader()
     keys, contents = reader.read(paths_queue)
@@ -23,9 +24,16 @@ if __name__ == '__main__':
     parser.add_argument('s3_path', type=str, help='path of the S3 endpoint.')
     args = parser.parse_args()
 
+    os.environ['AWS_REGION'] = '' 
+    os.environ['S3_ENDPOINT'] = 'rook-ceph-rgw-rooks3.rook' 
+    os.environ['S3_USE_HTTPS'] = "0" 
+    os.environ['S3_VERIFY_SSL'] = "0"
+    os.environ['TF_CPP_MIN_LOG_LEVEL']='0'
+
     with tf.Graph().as_default():
 
         batch = load_batch(args.s3_path)
+        
 
         with tf.Session() as sess:
 
